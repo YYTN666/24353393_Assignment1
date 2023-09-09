bfd9952c2d4b3ae844a2a670aa7950e3528fc83e
YYTN666 <yang93933@gmail.com>
2023-09-10 00:03:33 +1000

    Finish the task 2

--- Assignment1_Student Marking System/Assignment1.java
+++ Assignment1_Student Marking System/Assignment1.java
@@ -2,14 +2,32 @@
 /**
  * Write a description of class Assignment1 here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Yutaien Yang)
+ * @version (20230909)
  */
 import java.util.Scanner;
 public class Assignment1
 {
    public void ReceiveAssignmentName(){ //ask the user to input the name of assignmnet  
        Scanner scanner = new Scanner(System.in);
-       
+       System.out.print("Please enter the name of the assignment:");
+       String assignmentName = scanner.nextLine();
+       scanner.close();
    }
+   
+   public void ReceiveAssignmentMarks(){ //ask the user to input the marks of assignmnet  
+       double[] studentMarks = new double[30];
+       Scanner scanner = new Scanner(System.in);
+       for (int i = 0; i < 30; i++) {
+           System.out.print("Please enter the marks of the assignment " + ( i + 1 ) + ": ");
+           double mark = scanner.nextDouble();
+           if(mark > 30 || mark < 0) {
+               System.out.print("Invalid input! Mark should be between 0 and 30!");
+           } else {
+               studentMarks[i] = scanner.nextDouble();
+           }
+       }
+       scanner.close();
+   }
+
 }
