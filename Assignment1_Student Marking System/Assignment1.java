d1f8bb35ea9e692616f228b5f2d81de57943fa6a
YYTN666 <yang93933@gmail.com>
2023-09-09 22:54:23 +1000

    Start from task 1

---
+++ Assignment1_Student Marking System/Assignment1.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Assignment1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class Assignment1
+{
+   public void ReceiveAssignmentName(){ //ask the user to input the name of assignmnet  
+       Scanner scanner = new Scanner(System.in);
+       
+   }
+}
