b808e365c76ccb71c57691e93f1b24536514776a
YYTN666 <yang93933@gmail.com>
2023-09-10 01:13:33 +1000

    fix the fault1

--- Assignment1_Student Marking System/Assignment1.java
+++ Assignment1_Student Marking System/Assignment1.java
@@ -8,16 +8,13 @@
 import java.util.Scanner;
 public class Assignment1
 {
-   public void ReceiveAssignmentName(){ //ask the user to input the name of assignmnet  
+   public static void main (String[] args){ //ask the user to input the name of assignmnet  
        Scanner scanner = new Scanner(System.in);
        System.out.print("Please enter the name of the assignment:");
        String assignmentName = scanner.nextLine();
-       scanner.close();
-   }
-   
-   public void ReceiveAssignmentMarks(){ //ask the user to input the marks of assignmnet  
+       
+  //ask the user to input the marks of assignmnet  
        double[] studentMarks = new double[30];
-       Scanner scanner = new Scanner(System.in);
        for (int i = 0; i < 30; i++) {
            System.out.print("Please enter the marks of the assignment " + ( i + 1 ) + ": ");
            double mark = scanner.nextDouble();
