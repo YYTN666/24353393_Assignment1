bba982787309385ae4c143af4e7f3d19817390db
YYTN666 <yang93933@gmail.com>
2023-09-10 10:11:51 +1000

    fix the fault2
    1. Fixed the problem of not being able to open the programme
    2. fixed the function of checking invalid data
    3. fixed the problem that when entering student grades, you have to enter a number between two student grades to jump.

--- Assignment1_Student Marking System/Assignment1.java
+++ Assignment1_Student Marking System/Assignment1.java
@@ -8,21 +8,25 @@
 import java.util.Scanner;
 public class Assignment1
 {
-   public static void main (String[] args){ //ask the user to input the name of assignmnet  
+   public static void main (String[] args){
+       //Ask the user to input the name of assignmnet  
        Scanner scanner = new Scanner(System.in);
        System.out.print("Please enter the name of the assignment:");
        String assignmentName = scanner.nextLine();
        
-  //ask the user to input the marks of assignmnet  
+       
+       //Ask the user to input the marks of assignmnet  
        double[] studentMarks = new double[30];
        for (int i = 0; i < 30; i++) {
-           System.out.print("Please enter the marks of the assignment " + ( i + 1 ) + ": ");
+           while(true) {
+           System.out.print("Please enter the marks of the student " + ( i + 1 ) + ": ");
            double mark = scanner.nextDouble();
-           if(mark > 30 || mark < 0) {
-               System.out.print("Invalid input! Mark should be between 0 and 30!");
+           if(mark > 30 || mark < 0) {//Check for invalid data 
+               System.out.println("Invalid input! Mark should be between 0 and 30!");
            } else {
-               studentMarks[i] = scanner.nextDouble();
+               studentMarks[i] = mark; break;
            }
+        }
        }
        scanner.close();
    }
